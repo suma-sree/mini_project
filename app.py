bf0e4cfb01d0fc1c94c277a611c0495a331904f3
sai suma sree vankayalapati <vankayalapatisaisumasree2@gmail.com>
2025-04-10 21:21:50 +0530

    updated second time

--- app.py
+++ app.py
@@ -1,59 +1,4 @@
 
-# from flask import Flask, render_template, request
-# import pickle
-# import numpy as np
-
-# app = Flask(__name__)
-
-# # Load the trained model
-# model = pickle.load(open("data/model.pkl", "rb"))
-
-# @app.route("/")  # Main page route
-# def home():
-#     return render_template("home.html",prediction = None)  # Renders home page
-
-# @app.route("/predict_page")  # Route to prediction page
-# def predict_page():
-#     return render_template("p.html" , prediction = None)
-
-# @app.route("/predict", methods=["POST"])  # Prediction logic
-# def predict():
-#     try:
-#         # Get user inputs from form
-#         features = [
-#             request.form["age_band"],
-#             request.form["vehicle_type"],
-#             request.form["road_surface"],
-#             request.form["light_conditions"],
-#             request.form["weather"],
-#             request.form["collision_type"],
-#             request.form["casualties"],
-#             request.form["pedestrian_movement"],
-#             request.form["hour"],
-#             request.form["day_of_week"],
-#             request.form["cause_of_accident"],
-#             request.form["gender"]
-#         ]
-
-#         # Convert input data into NumPy array
-#         features_array = np.array(features).reshape(1, -1)
-
-#         # Make prediction
-#         # prediction = model.predict(features_array)[0]
-#         prediction = int(model.predict(features_array)[0])
-
-#         print("Predicted class:", prediction)
-
-
-#         return render_template("p.html", prediction=prediction)
-
-#     except Exception as e:
-#         return render_template("p.html", prediction=f"Error: {str(e)}")
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-    
-
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
@@ -61,7 +6,7 @@ import numpy as np
 app = Flask(__name__)
 
 # Load the trained model
-model = pickle.load(open("data/model.pkl", "rb"))
+model = pickle.load(open("data/random_forest_model.pkl", "rb"))
 
 @app.route("/")
 def home():
@@ -69,7 +14,7 @@ def home():
 
 @app.route("/predict_page")
 def predict_page():
-    return render_template("p.html")
+    return render_template("form.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
